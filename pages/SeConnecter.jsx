c19335917d7e03440376ea108e13d1b0c034b11c
aymane lagmihi <ayman.lagmihi.123@gmail.com>
2023-04-09 22:49:50 +0000

    #

--- pages/SeConnecter.jsx
+++ pages/SeConnecter.jsx
@@ -8,43 +8,42 @@ import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRound
 export default function SeConnecter() {
     return(
     <>
-    <div className={styles.loginPage}>
-        
-        <div className={styles.leftSide}>
-            <div className={styles.header}>
-                <Image src={logo} className={styles.logo} alt='logo'/>
-            </div>
-
-            <div className={styles.imageIllustration}>
-                <Image src={illustration} className={styles.illustration} alt='illustration'/>
-            </div>
-        </div>
-
-        <div className={styles.loginSide}>
-            <Link href="/"  className={styles.backIcon}>
-                <ArrowBackIosNewRoundedIcon  className={styles.arrow} sx={{ color: '#008B45' }}/>
-            </Link>
-            <div className={styles.containerForm}>
-                <div className={styles.textSide}>
-                    <h1>Se connecter</h1>
-                    <p>Content de te revoir  Veuillez saisir vos cordonnées.</p> 
+        <div className={styles.loginPage}>
+            <div className={styles.leftSide}>
+                <div className={styles.header}>
+                    <Image src={logo} className={styles.logo} alt='logo'/>
                 </div>
-                <div className={styles.allInputs}> 
-                    <div className={styles.div}>
-                        <i className={styles.circl}></i>
-                        <input className={styles.input} type="email" placeholder="Entrez votre adresse e-mail" /> 
-                    </div>
-                    <div className={styles.div}> 
-                        <i className={styles.circl}></i>
-                        <input className={styles.input} type="password" placeholder="Entrez votre mot de passe" minlength="8"/>  
-                    </div>
-                    <div className={styles.button1}>
-                        <button className={styles.button}>Se connecter</button> 
-                    </div>
+
+                <div className={styles.imageIllustration}>
+                    <Image src={illustration} className={styles.illustration} alt='illustration'/>
                 </div>
             </div>
+
+            <div className={styles.loginSide}>
+                <Link href="/"  className={styles.backIcon}>
+                    <ArrowBackIosNewRoundedIcon  className={styles.arrow} sx={{ color: '#008B45' }}/>
+                </Link>
+                <div className={styles.containerForm}>
+                    <div className={styles.textSide}>
+                        <h1>Se connecter</h1>
+                        <p>Content de te revoir  Veuillez saisir vos cordonnées.</p> 
+                    </div>
+                    <div className={styles.allInputs}> 
+                        <div className={styles.div}>
+                            <i className={styles.circl}></i>
+                            <input className={styles.input} type="email" placeholder="Entrez votre adresse e-mail" /> 
+                        </div>
+                        <div className={styles.div}> 
+                            <i className={styles.circl}></i>
+                            <input className={styles.input} type="password" placeholder="Entrez votre mot de passe" minlength="8"/>  
+                        </div>
+                        <div className={styles.button1}>
+                            <button className={styles.button}>Se connecter</button> 
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     </>
     )
 }
